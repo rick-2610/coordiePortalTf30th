9721b51da7fd3c3ec6ef7b48e7da479374832c2b
unknown <24b0059@iitb.ac.in>
2026-04-09 06:41:57 +0530

    smal change

--- backend/player/urls.py
+++ backend/player/urls.py
@@ -4,7 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('top/', top_scores_list, name='top-scores'),
-    path('create/', create_score, name='create-score'),
     path('live/', live_update, name='live-update'),
     path('create/', create_player, name='create-player'),
     path('<int:pk>/update_score/', update_player_score, name='update-player-score'),
