895f611f7af16af30a220b3a10fab86fcfd8d47d
unknown <24b0059@iitb.ac.in>
2026-04-22 11:42:32 +0530

    cnavas fix

--- frontend/src/pages/game/Game.jsx
+++ frontend/src/pages/game/Game.jsx
@@ -482,8 +482,6 @@ const Game = ({
                     className="game-over"
                     style={{
                         position: "absolute",
-                        top: 0,
-                        left: 0,
                         width: "100%",
                         height: "100%",
                         display: "flex",
