c557aedf86dee739ea590ae6afc7bf37c4ff0e4d
unknown <24b0059@iitb.ac.in>
2026-04-22 11:42:58 +0530

    canvas fix

--- frontend/src/pages/game/Game.jsx
+++ frontend/src/pages/game/Game.jsx
@@ -461,8 +461,6 @@ const Game = ({
                     className="start-screen"
                     style={{
                         position: "absolute",
-                        top: 0,
-                        left: 0,
                         width: "100%",
                         height: "100%",
                         display: "flex",
